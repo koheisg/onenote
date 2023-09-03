d6b0bd3822000db001e82045889e3be596384959
Kohei SUGI <koheisg@hey.com>
2023-09-03 23:26:29 +0900

    Add note

---
+++ db/migrate/20230903142556_create_notes.rb
@@ -0,0 +1,9 @@
+class CreateNotes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notes do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
